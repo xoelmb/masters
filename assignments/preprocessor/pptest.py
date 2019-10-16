63fd8664da16359f4550198d7aa7b3e7027c2998
xoelmb <xoelmb@gmail.com>
2019-10-16 17:31:56 +0200

    new samples, rc completed, removed old version

--- assignments/preprocessor/pptest.py
+++ assignments/preprocessor/pptest.py
@@ -1,35 +1,80 @@
-from typing import Dict, Any
-
-
 def get_par(args):
-    parameters ={}
+    parameters = {}
     for i in range(len(args)):
         if args[i][0:2:] == "--":
-            parameters[args[i][2::]] = args[i+1]
+            parameters[args[i][2::]] = args[i + 1]
     return parameters
 
 
 def check_par(parameters):
-
     print("Cal fer una funciò que comprobi que els parameters estan bé introduits.")
 
     return True
 
 
+def get_format(input_file):
+    fp = open(input_file, "r")
+    for line in fp:
+        if line[0] == ">":
+            file_format = "FASTA"
+        elif line[0] == "@":
+            file_format = "FASTQ"
+        else:
+            file_format= False
+        break
+    fp.close()
+    return file_format
 
 
-test_arguments=["name.py", "--input", "input.fasta", "--output", "output_file.fa", "--operation", "rc"]
+def revcomp(input_file, output_file, file_format):
+    comp_dic = {"A": "T", "C": "G", "G":"C", "T":"A", "N":"N", "a": "t", "c": "g", "g":"c", "t":"a", "n":"n"}
+    new_file = open(output_file,"wt")
+    with open(input_file, "r") as fp:
+        while True:
+            tag = fp.readline().rstrip()
+            if not tag:
+                break
+            sequence = fp.readline().rstrip()
+            try:
+                revcomp = "".join(comp_dic[base] for base in sequence[::-1])
+                new_file.write(tag+"\n"+revcomp+"\n")
+                if file_format=="FASTQ":
+                    fp.readline()
+                    qual = fp.readline().rstrip()
+                    revqual = "".join(qual[::-1])
+                    new_file.write("+\n" + revqual + "\n")
+            except:
+                print(tag,"could not be processed.")
+                if file_format == "FASTQ":
+                    fp.readline()
+                    fp.readline()
+
+
+def trim(file, tag):
+    print("haha")
+    return "okay"
+
+
+def adaptor_removal(file,tag):
+    print("hihi")
+    return "okay"
+
+
+
+test_arguments = ["name.py", "--input", "mbio.sample.fastq", "--output", "output_file.fastq", "--operation", "rc", "jeje"]
+
 
 parameters = get_par(test_arguments)
 
-print(parameters)
 
-if check_par(parameters)==False:
+if check_par(parameters) == False:
     print("Invalid arguments used. Exiting.")
-    quit()
+    exit(1)
 
-with open(parameters["input"], "r") as fp:
-    no_line = 0
-    for line in fp:
-        no_line += 1
-        print("Line %d: %s" % (no_line,line))
+
+file_format=get_format(parameters["input"])
+if file_format == False:
+    print("Invalid file format. Exiting.")
+
+
+revcomp(parameters["input"], parameters["output"], file_format)
