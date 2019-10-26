220a6053f8de2a507387108ec7be4d78cec4589e
xoel <xoelmb@gmail.com>
2019-10-26 19:34:58 +0200

    add: intro; fix: trim and adaptor for shorter-than-parameters seqs

--- module_i/assignments/preprocessor/pptest.py
+++ module_i/assignments/preprocessor/pptest.py
@@ -1,3 +1,12 @@
+# The script below codes for a FASTX preprocessor that can reverse-complement, trim and remove adaptors for both FASTA
+# and FASTQ files. The code has been designed to be versatile, modular and to be used even if the input arguments are
+# slightly messy. For example, it can not only perform trimming if both left and right parameters are established, but
+# it can also perform right- or left-only trimming to sequences. Due to that, some extra code and variables had to be
+# written, but I think the trade-off is worth it since it makes the script more robust overall.
+# The use of classes allowed me to create quite flexible functions and to record and update the statistics along the
+# script with the use of only one object and its defined methods.
+
+
 import re
 import sys
 
@@ -8,7 +17,7 @@ class Read:  # Class used to store sequence and quality of a given read
         self.quality = qual
 
 
-class Summary:  # Class used to store different data of the whole operation
+class Summary:  # Class used to store different statistics of the whole operation
     def __init__(self):
         self.bases_dic = {"A": 0, "C": 0, "G": 0, "T": 0, "N": 0}
         self.bases_total = 0
@@ -24,14 +33,17 @@ class Summary:  # Class used to store different data of the whole operation
                 self.bases_dic[base] += 1
             self.bases_total += 1
 
-    def add_trim(self, seq, left, right):  # Update statistics when a trim operation is performed
+    def add_trim(self, seq, left, right, total):  # Update statistics when a trim operation is performed
         seq_left = ""
         seq_right = ""
-        if left != 0:
-            seq_left = seq[:left:]
-        if right is not None:
-            seq_right = seq[right::]
-        removed = seq_left + seq_right
+        if len(seq) > total:
+            if left != 0:
+                seq_left = seq[:left:]
+            if right is not None:
+                seq_right = seq[right::]
+            removed = seq_left + seq_right
+        else:
+            removed = seq
         for base in removed[::].upper():
             if base in self.trim_dic.keys():
                 self.trim_dic[base] += 1
@@ -41,7 +53,8 @@ class Summary:  # Class used to store different data of the whole operation
         self.adaptors += 1
 
     def report(self, mode):  # Function to print statistics depending on the operation performed
-        print("Summary:\n" + (len(str(self.bases_total))-len(str(self.seqs)))*" " + str(self.seqs), "reads processed\n" + str(self.bases_total), "bases processed (",
+        print("Summary:\n" + (len(str(self.bases_total)) - len(str(self.seqs))) * " " + str(self.seqs),
+              "reads processed\n" + str(self.bases_total), "bases processed (",
               end="")
         for base in ["A", "C", "G", "T", "N"]:
             print(str(int(self.bases_dic[base] / self.bases_total * 100)) + "%", base, end="")
@@ -50,7 +63,8 @@ class Summary:  # Class used to store different data of the whole operation
             else:
                 print(")")
         if mode == "trim":
-            print((len(str(self.bases_total))-len(str(self.trim_total)))*" " + str(self.trim_total), "bases trimmed (", end="")
+            print((len(str(self.bases_total)) - len(str(self.trim_total))) * " " + str(self.trim_total),
+                  "bases trimmed (", end="")
             for base in ["A", "C", "G", "T", "N"]:
                 print(str(int(self.trim_dic[base] / self.trim_total * 100)) + "%", base, end="")
                 if base != "N":
@@ -58,7 +72,7 @@ class Summary:  # Class used to store different data of the whole operation
                 else:
                     print(")")
         elif mode == "adaptor_removal":
-            print((len(str(self.bases_total))-len(str(self.adaptors)))*" " + str(self.adaptors) + " adaptors found")
+            print((len(str(self.bases_total)) - len(str(self.adaptors))) * " " + str(self.adaptors) + " adaptors found")
 
 
 def get_par(args):  # Function to parse the arguments used by the user
@@ -69,7 +83,7 @@ def get_par(args):  # Function to parse the arguments used by the user
             pars[args[i][2::]] = args[i + 1]  # Stores the next argument as the value of a given parameter
             last = args[i + 1]  # Updates the last value stored
         elif args[i] != last:  # If an argument has not been preceded by a "--parameter", it's ignored
-            print(args[i]+": missing operator")
+            print(args[i] + ": missing operator")
     return pars  # Returns the whole dictionary of parameters
 
 
@@ -144,13 +158,14 @@ def revcomp(unprocessed, stats):  # Performs reverse-complement on a given seque
         return None
 
 
-def trim(unprocessed, left, right, stats):  # Performs trimming on a given sequence and quality if needed
+def trim(unprocessed, left, right, total, stats):  # Performs trimming on a given sequence and quality if needed
     processed = Read()  # Initializes the resulting read
-    processed.sequence = unprocessed.sequence[left:right:]  # Trims the sequence
-    if unprocessed.quality:  # Trims the qualities if the read contains any
-        processed.quality = unprocessed.quality[left:right:]
+    if len(unprocessed.sequence) > total:
+        processed.sequence = unprocessed.sequence[left:right:]  # Trims the sequence
+        if unprocessed.quality:  # Trims the qualities if the read contains any
+            processed.quality = unprocessed.quality[left:right:]
     stats.add_seq(unprocessed.sequence)  # Updates statistics
-    stats.add_trim(unprocessed.sequence, left, right)  # Updates trimming statistics
+    stats.add_trim(unprocessed.sequence, left, right, total)  # Updates trimming statistics
     return processed  # Returns a processed read
 
 
@@ -197,22 +212,23 @@ with open(parameters["input"], "rt") as fp:  # Opens the input file
         # The following lines perform the necessary operation on the read and stores the result
         if parameters["operation"] == "rc":
             processed_read = revcomp(current_read, content)
-        elif parameters["operation"] == "trim" and len(sequence) > parameters["trim-total"]:
-            processed_read = trim(current_read, parameters["trim-left"], parameters["trim-right"], content)
-        elif parameters["operation"] == "adaptor_removal" and len(sequence) > len(parameters["adaptor"]):
+        elif parameters["operation"] == "trim":
+            processed_read = trim(current_read, parameters["trim-left"], parameters["trim-right"], parameters["trim-total"], content)
+        elif parameters["operation"] == "adaptor_removal":
             processed_read = adaptor_removal(current_read, parameters["adaptor"], content)
         else:
             processed_read = None
 
         if processed_read:  # If the read has been processed, it writes the result on the new file
-            new_file.write(tag + "\n" + processed_read.sequence + "\n")
-            if processed_read.quality:  # If it is a FASTQ file, the processed qualities are present
-                new_file.write("+\n" + processed_read.quality + "\n")  # The processed qualities are written on the file
+            if processed_read.sequence:  # Only if the sequence has not been completely trimmed/deleted
+                new_file.write(tag + "\n" + processed_read.sequence + "\n")
+                if processed_read.quality:  # If it is a FASTQ file, the processed qualities are present
+                    new_file.write("+\n" + processed_read.quality + "\n")  # Writes processed qualities on the file
         else:  # If the operation could not be completed, the read is discarded and a warning is raised
             print(tag, "could not be processed.")
 
 new_file.close()  # Closes the new file
 final_print = {"rc": "reversed-complemented", "adaptor_removal": "processed", "trim": "hard-trimmed"}
-print("\nFile '" + parameters["input"] + "' has been successfully %s " % final_print[parameters["operation"]] + "('" + parameters["output"] + "')" )
+print("\nFile '" + parameters["input"] + "' has been successfully %s " % final_print[parameters["operation"]] + "('" + parameters["output"] + "')")
 content.report(parameters["operation"])  # Prints the summary of the operation
 exit(0)
