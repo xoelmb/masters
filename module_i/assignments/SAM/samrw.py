967c370804b0acf6fb423b6765f5d972822e40f2
xoel <xoelmb@gmail.com>
2020-01-05 15:19:47 +0100

    some progress

--- module_i/assignments/SAM/samrw.py
+++ module_i/assignments/SAM/samrw.py
@@ -24,7 +24,7 @@ def open_sam(filename):
     return heads, recs
 
 
-def compare_records(first, second):
+def compare_records(first, second, pos_field=3):
     chr_order = {'chr1': 1, 'chr2': 2, 'chr3': 3, 'chr4': 4, 'chr5': 5, 'chr6': 6, 'chr7': 7, 'chr8': 8, 'chr9': 9,
                  'chr10': 10, 'chr11': 11, 'chr12': 12, 'chr13': 13, 'chr14': 14, 'chr15': 15, 'chr16': 16, 'chr17': 17,
                  'chr18': 18, 'chr19': 19, 'chr20': 20, 'chr21': 21, 'chr22': 22, 'chrX': 23, 'chrY': 24, '*': 25}
@@ -34,22 +34,22 @@ def compare_records(first, second):
     elif chr_order[first[2]] > chr_order[second[2]]:
         return 2
     else:
-        if first[3] == '*':
+        if first[pos_field] == '*':
             return 2
-        elif second[3] == '*':
+        elif second[pos_field] == '*':
             return 1
-        elif first[3] == '*' and second[3] == '*':
+        elif first[pos_field] == '*' and second[pos_field] == '*':
             return 0
         else:
-            if int(first[3]) < int(second[3]):
+            if int(first[pos_field]) < int(second[pos_field]):
                 return 1
-            elif int(first[3]) > int(second[3]):
+            elif int(first[pos_field]) > int(second[pos_field]):
                 return 2
             else:
                 return 0
 
 
-def merge_sort(recordlist):
+def merge_sort(recordlist, pos_field=3):
     # Checks if the length of the list is greater than 1
     if len(recordlist) > 1:
         # Compute middle point
@@ -58,16 +58,16 @@ def merge_sort(recordlist):
         right_list = recordlist[t:]
         merge_sort(left_list)
         merge_sort(right_list)
-        merge_lists(left_list, right_list, recordlist)
+        merge_lists(left_list, right_list, recordlist, pos_field)
     return recordlist
 
 
-def merge_lists(list1, list2, merged):
+def merge_lists(list1, list2, merged, pos_field=3):
     p1 = 0
     p2 = 0
     pmerged = 0
     while p1 < len(list1) and p2 < len(list2):
-        if compare_records(list1[p1], list2[p2]) < 2:
+        if compare_records(list1[p1], list2[p2], pos_field) < 2:
             merged[pmerged] = list1[p1]
             p1 += 1
         else:
@@ -86,14 +86,14 @@ def merge_lists(list1, list2, merged):
         pmerged += 1
 
 
-def bin_search(rec_list, item):
+def bin_search(rec_list, item, pos_field=3):
     if len(rec_list) > 1:
         middle = len(rec_list) // 2
-        comparison = compare_records(rec_list[middle], item)
+        comparison = compare_records(rec_list[middle], item, pos_field)
         if comparison == 0:
             return middle
         elif comparison == 1:
-            return middle + bin_search(rec_list[middle::], item)
+            return middle + bin_search(rec_list[middle::], item, pos_field)
         else:
             return bin_search(rec_list[:middle:], item)
     elif len(rec_list) == 1:
@@ -121,7 +121,7 @@ headers, records = open_sam(pars["input"])
 
 merge_sort(records)
 
-with open('sorted_'+pars["input"], 'wt') as op:
+with open('sorted_' + pars["input"], 'wt') as op:
     for header in headers:
         op.write(header)
     for record in records:
@@ -130,7 +130,7 @@ with open('sorted_'+pars["input"], 'wt') as op:
 start_idx = bin_search(records, pars['from'])
 end_idx = bin_search(records, pars['to'])
 
-with open('found_'+pars["input"], 'wt') as op:
+with open('found_' + pars["input"], 'wt') as op:
     for header in headers:
         op.write(header)
     for record in records[start_idx:end_idx]:
