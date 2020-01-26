91333fc51da1cd65a8ba90defbb3964cf9277bc3
xoel <xoelmb@gmail.com>
2020-01-26 20:03:43 +0100

    str

--- slurm/dependencies.py
+++ slurm/dependencies.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 import subprocess
+
 print("Start dependent job at", datetime.today())
 
 # first job - no dependencies
@@ -19,7 +20,8 @@ jid3 = str(subprocess.check_output(f"sbatch  --partition=research.q --dependency
 jid3 = ''.join([c for c in jid3 if c.isnumeric()])
 
 # a single job can depend on multiple jobs
-jid4 = str(subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterany:{jid2}:{jid3} job4.slurm".split()))
+jid4 = str(
+    subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterany:{jid2}:{jid3} job4.slurm".split()))
 jid4 = ''.join([c for c in jid4 if c.isnumeric()])
 jid5 = str(subprocess.check_output(f"sbatch --partition=research.q --dependency=afterany:{jid4} job5.slurm".split()))
 jid5 = ''.join([c for c in jid5 if c.isnumeric()])
@@ -27,7 +29,8 @@ jid5 = ''.join([c for c in jid5 if c.isnumeric()])
 # show dependencies in squeue output:
 user = str(subprocess.check_output(['whoami']))[2:-3]
 format_op = '"%.8A %.4C %.10m %.20E"'
-command = "f'squeue -u {user} -o '".split()
+command = 'squeue -u ' + user + ' -o '
+command = command.split()
 command = command.append(format_op)
 print(command)
 print(subprocess.check_output(command).output)
