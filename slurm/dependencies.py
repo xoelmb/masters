ab91634fd9e17e0cab0f27f54948f078be05a4b5
xoel <xoelmb@gmail.com>
2020-01-26 19:29:01 +0100

    str

--- slurm/dependencies.py
+++ slurm/dependencies.py
@@ -13,15 +13,15 @@ jid1 = ''.join([c for c in jid1 if c.isnumeric()])
 print(jid1)
 
 # multiple jobs can depend on a single job
-jid2 = subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterok:{jid1}:job2.slurm".split())
+jid2 = str(subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterok:{jid1}:job2.slurm".split()))
 jid2 = ''.join([c for c in jid2 if c.isnumeric()])
-jid3 = subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterok:{jid1} job3.slurm".split())
+jid3 = str(subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterok:{jid1} job3.slurm".split()))
 jid3 = ''.join([c for c in jid3 if c.isnumeric()])
 
 # a single job can depend on multiple jobs
-jid4 = subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterany:{jid2}:{jid3} job4.slurm".split())
+jid4 = str(subprocess.check_output(f"sbatch  --partition=research.q --dependency=afterany:{jid2}:{jid3} job4.slurm".split()))
 jid4 = ''.join([c for c in jid4 if c.isnumeric()])
-jid5 = subprocess.check_output(f"sbatch --partition=research.q --dependency=afterany:{jid4} job5.slurm".split())
+jid5 = str(subprocess.check_output(f"sbatch --partition=research.q --dependency=afterany:{jid4} job5.slurm".split()))
 jid5 = ''.join([c for c in jid5 if c.isnumeric()])
 
 # show dependencies in squeue output:
