8b5f78d640de6da83ec956bbaccfebd7521eab55
xoel <xoelmb@gmail.com>
2020-01-26 18:39:11 +0100

    wip

--- slurm/dependencies.py
+++ slurm/dependencies.py
@@ -8,23 +8,29 @@ print("Start dependent job at", datetime.today())
 
 print(datetime.today())
 
-jid1 = subprocess.run("sbatch --partition=research.q job1.slurm | cut -f 4 -d' '".split(" "), capture_output=)
-
-jid1=$(sbatch --partition=research.q job1.slurm | cut -f 4 -d' ')
-
-echo $jid1
-
-# multiple jobs can depend on a single job
-jid2=$(sbatch  --partition=research.q --dependency=afterok:$jid1 job2.slurm | cut -f 4 -d' ')
-jid3=$(sbatch  --partition=research.q --dependency=afterok:$jid1 job3.slurm | cut -f 4 -d' ')
-
-# a single job can depend on multiple jobs
-jid4=$(sbatch  --partition=research.q --dependency=afterany:$jid2:$jid3 job4.slurm | cut -f 4 -d' ')
-
-jid5=$(sbatch --partition=research.q --dependency=afterany:$jid4 job5.slurm | cut -f 4 -d' ')
-
-# show dependencies in squeue output:
-squeue -u $USER -o "%.8A %.4C %.10m %.20E"
-
-# print final date and time
-echo "End dependent job at $(date)"
+jid1 = subprocess.run("sbatch --partition=research.q job1.slurm".split(" "), capture_output=True).stdout()
+jid1 = [c for c in jid1 if c.isnumeric()]
+print(jid1)
+#
+#
+#
+#
+#
+# jid1=$(sbatch --partition=research.q job1.slurm | cut -f 4 -d' ')
+#
+# echo $jid1
+#
+# # multiple jobs can depend on a single job
+# jid2=$(sbatch  --partition=research.q --dependency=afterok:$jid1 job2.slurm | cut -f 4 -d' ')
+# jid3=$(sbatch  --partition=research.q --dependency=afterok:$jid1 job3.slurm | cut -f 4 -d' ')
+#
+# # a single job can depend on multiple jobs
+# jid4=$(sbatch  --partition=research.q --dependency=afterany:$jid2:$jid3 job4.slurm | cut -f 4 -d' ')
+#
+# jid5=$(sbatch --partition=research.q --dependency=afterany:$jid4 job5.slurm | cut -f 4 -d' ')
+#
+# # show dependencies in squeue output:
+# squeue -u $USER -o "%.8A %.4C %.10m %.20E"
+#
+# # print final date and time
+# echo "End dependent job at $(date)"
