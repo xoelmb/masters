66c1196a142f061521add96f85f738ead444f080
xoel <xoelmb@gmail.com>
2020-01-27 21:18:02 +0100

    wip

--- slurm/bwa_example.sh
+++ slurm/bwa_example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/usr/bin/bwa index example1.fastq &>/dev/null
-/usr/bin/bwa index example2.fastq &>/dev/null
-/usr/bin/bwa index example3.fastq &>/dev/null
+/usr/bin/bwa index example1.fastq
+/usr/bin/bwa index example2.fastq
+/usr/bin/bwa index example3.fastq
+rm example*.fastq.*
 echo "All processes completed"
